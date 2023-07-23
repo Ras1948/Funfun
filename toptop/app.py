58c139f385284eb21561297003b8a28ad9d19eb7
Rasmus <rasmus.burvall@microsoft.com>
2023-07-23 21:28:19 +0200

    ras

--- toptop/app.py
+++ toptop/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
 
 @app.route('/')
-def hello():
-    return "Hello, Flask!"
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
